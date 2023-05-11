f1ddfa0fc295f05b532f9cbd03fafca3ce9f0060
Johanny A. Mateo <24462857+jmateocodes@users.noreply.github.com>
2023-05-11 00:56:42 -0400

    1.09 insert record into customer table

--- 01_restaurant/challenges/01_09_createreservation_ch.sql
+++ 01_restaurant/challenges/01_09_createreservation_ch.sql
@@ -1,6 +1,12 @@
 -- Create a reservation for a customer who may or may not
 -- already be listed in our Customers table.
-
 -- Use the following information:
 -- Sam McAdams (smac@kinetecoinc.com), for 5 people
 -- on August 12, 2022 at 6PM (18:00)
+-- check if they're in the customer table
+SELECT *
+FROM Customers
+WHERE Email = "smac@kinetecoinc.com";
+-- Add customer
+INSERT INTO Customers (FirstName, LastName, Email)
+VALUES("Sam", "McAdams", "smac@kinetecoinc.com");
