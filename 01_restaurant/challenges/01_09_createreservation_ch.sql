d159859a3a9a8be8f143a3101e38709f8dd059d8
Johanny A. Mateo <24462857+jmateocodes@users.noreply.github.com>
2023-05-11 01:03:51 -0400

    complete 1.09

--- 01_restaurant/challenges/01_09_createreservation_ch.sql
+++ 01_restaurant/challenges/01_09_createreservation_ch.sql
@@ -10,3 +10,21 @@ WHERE Email = "smac@kinetecoinc.com";
 -- Add customer
 INSERT INTO Customers (FirstName, LastName, Email)
 VALUES("Sam", "McAdams", "smac@kinetecoinc.com");
+UPDATE Customers
+SET Phone = "555-555-1232"
+WHERE Email = "smac@kinetecoinc.com";
+-- Add reservation
+INSERT INTO Reservations(CustomerID, Date, PartySize)
+VALUES (
+    (
+      SELECT CustomerID
+      FROM Customers
+      WHERE Email = "smac@kinetecoinc.com"
+    ),
+    "2022-08-12 18:00:00",
+    5
+  );
+SELECT *
+FROM Reservations
+  JOIN Customers On Customers.CustomerID = Reservations.CustomerID
+WHERE Email = "smac@kinetecoinc.com";
