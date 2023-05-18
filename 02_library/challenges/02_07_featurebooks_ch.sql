96a3877f262ef22e0e754ab86a8d138e28609e29
Johanny A. Mateo <24462857+jmateocodes@users.noreply.github.com>
2023-05-17 22:53:41 -0400

    complete

--- 02_library/challenges/02_07_featurebooks_ch.sql
+++ 02_library/challenges/02_07_featurebooks_ch.sql
@@ -1,4 +1,15 @@
 -- Create a list of books to feature in an exhibit.
-
 -- Make a pick list of books published from 1890-1899 
 -- which are not currently checked out.
+SELECT Books.Title,
+  Books.Author,
+  Books.Published
+FROM Books
+WHERE Published BETWEEN 1890 AND 1899
+  AND (
+    BookID NOT IN (
+      SELECT BookID
+      FROM Loans
+      WHERE ReturnedDate IS NULL
+    )
+  );
--- 02_library/challenges/02_08_bookstats_ch.sql
+++ 02_library/challenges/02_08_bookstats_ch.sql
@@ -1,7 +1,18 @@
 -- Create two reports about book statistics.
-
 -- Report 1: Show how many titles were published 
 -- in each year.
-
 -- Report 2: Show the five books that have been
 -- checked out the most.
+SELECT Published,
+  Count(Title) Published_Cnt
+FROM Books
+GROUP BY Published
+ORDER BY Published_Cnt DESC;
+SELECT Title,
+  Author,
+  count(Loans.BookID) Loan_Cnt
+FROM Loans
+  JOIN Books ON Books.BookID = Loans.BookID
+GROUP BY Title
+ORDER BY Loan_Cnt DESC
+LIMIT 5;
