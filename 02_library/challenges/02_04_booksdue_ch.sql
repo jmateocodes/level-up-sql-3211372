5d852d2032b246156f1cbb89b6c511aed72d72b7
Johanny A. Mateo <24462857+jmateocodes@users.noreply.github.com>
2023-05-17 21:40:49 -0400

    completed 2.03-2.04

--- 02_library/challenges/02_04_booksdue_ch.sql
+++ 02_library/challenges/02_04_booksdue_ch.sql
@@ -2,3 +2,12 @@
 -- to the library on July 13, 2022.
 -- Provide the due date, the book title, and
 -- the borrower's first name and email address.
+SELECT FirstName,
+  Email,
+  Title,
+  DueDate
+FROM Loans
+  JOIN Books ON Books.BookID = Loans.BookID
+  JOIN Patrons ON Patrons.PatronID = Loans.PatronID
+WHERE DueDate = "2022-07-13"
+  AND ReturnedDate IS NULL
